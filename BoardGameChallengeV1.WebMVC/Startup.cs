d545476baffb61f55ebf7be8eda272af37ff6a5e
Joshua Ziegler <joshua.ziegler83@gmail.com>
2021-11-01 08:14:08 -0400

    Add project files.

---
+++ BoardGameChallengeV1.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BoardGameChallengeV1.WebMVC.Startup))]
+namespace BoardGameChallengeV1.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
