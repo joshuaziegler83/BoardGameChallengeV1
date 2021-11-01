a360f3f5766d9ea0f4d310fc762ea992a38ec137
Joshua Ziegler <joshua.ziegler83@gmail.com>
2021-11-01 08:38:17 -0400

    First commit

---
+++ BoardGameChallengeV1.Data/Play.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoardGameChallengeV1.Data
+{
+    public class Play
+    {
+        [Key]
+        public int PlayId { get; set; }
+        [Required]
+        public int UserId { get; set; }
+        [Required]
+        public int BoardGameId { get; set; }
+        [Required]
+        public int TimesPlayed { get; set; }
+        public string? Review { get; set; }
+        [DefaultValue(false)]
+        public bool IsReviewPrivate { get; set; }
+        public double Rating { get; set; }
+    }
+}
